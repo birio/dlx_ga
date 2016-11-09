0dbd0c87d20e814b3bfd6a3925a3e03e97d7d702
alberto guasco <albebirio@gmail.com>
2016-11-09 19:07:20 +0100

    add class cell and check if there is a user test passed as argument

--- sim/random_gen/generator.py
+++ sim/random_gen/generator.py
@@ -2,44 +2,49 @@
 
 import sys
 import random
+import importlib
 
 # generator includes
 import opcodes
 import weights
 
 # ./generator.py SEED
-
 seed = sys.argv[1]
-
 random.seed(seed)
 
-number_of_lines = 100
+# if argv[2] is present, import the python file associated
+if len(sys.argv) == 3:
+   var = sys.argv[2]
+   importlib.import_module(var, package=None)
 
-trues_l  = [True]  * weights.numb_of_regs_to_use 
-falses_l = [False] * (32 - weights.numb_of_regs_to_use)
+# fixed value
+number_of_lines = 10
+
+# available registers to use in the test
+trues_l  = [True]  * weights.test_weights_d["numb_of_regs_to_use"]
+falses_l = [False] * (32 - weights.test_weights_d["numb_of_regs_to_use"])
 prob_use_reg_l = trues_l + falses_l
 random.shuffle(prob_use_reg_l)
 available_regs_l = [ i for i in range(0, 32) if prob_use_reg_l[i] == True ]
 
+# labels used in the test
 num_of_labels = random.choice([i for i in range(0, weights.max_num_of_labels)])
 labels_lines_l = random.sample([i for i in range(0, number_of_lines)], num_of_labels)
 labels_lines_l.sort()
 printed_label = 0
-
-print "num_of_labels "
-print num_of_labels
-print "labels_lines_l "
-print labels_lines_l
-print labels_lines_l.__len__()
 
 print "available_regs_l"
 print available_regs_l 
 
 print "number_of_lines = " + str(number_of_lines)
 
+weights.set_list()
+print weights.weighted_opcodes_l 
+
 test_file = open('generated_test.asm', 'w+')
 
 for i in range (0, number_of_lines):
+   # chose an instr from the weighted list
    asm_line = random.sample(weights.weighted_opcodes_l, 1)
  
    if str(asm_line[0]) in opcodes.r_types_l:
@@ -52,21 +57,21 @@ for i in range (0, number_of_lines):
       r0 = random.choice(available_regs_l)
       r1 = random.choice(available_regs_l)
       if (asm_line[0] == "slti") or (asm_line[0] == "sltiu"):
-         imm = random.choice(weights.imm_value_15_l)
+         imm = random.choice(weights.test_weights_d["imm_value_15_l"])
       else:
-         imm = random.choice(weights.imm_value_16_l)
+         imm = random.choice(weights.test_weights_d["imm_value_16_l"])
       line = str(asm_line[0]) + "\t $" + str(r0) + ",\t $" + str(r1) + ",\t " + str(imm)
 
    elif str(asm_line[0]) in opcodes.s_i_types_l:
       r0 = random.choice(available_regs_l)
       r1 = random.choice(available_regs_l)
-      imm = random.choice(weights.imm_value_5_l)
+      imm = random.choice(weights.test_weights_d["imm_value_5_l"])
       line = str(asm_line[0]) + "\t $" + str(r0) + ",\t $" + str(r1) + ",\t " + str(imm)
 
    elif str(asm_line[0]) in opcodes.load_types_l:
       r0 = random.choice(available_regs_l)
       r1 = random.choice(available_regs_l)
-      imm = random.choice(weights.imm_value_16_l)
+      imm = random.choice(weights.test_weights_d["imm_value_16_l"])
       line = str(asm_line[0]) + "\t $" + str(r0) + ",\t " + str(imm) + "($" + str(r1) + ")"
 
    elif str(asm_line[0]) in opcodes.b_types_l:
@@ -78,7 +83,7 @@ for i in range (0, number_of_lines):
    elif str(asm_line[0]) in opcodes.l_i_types_l:
       r0 = random.choice(available_regs_l)
       if (asm_line[0] == "lui"):
-         imm = random.choice(weights.imm_value_16_l)
+         imm = random.choice(weights.test_weights_d["imm_value_16_l"])
       else: # "li"
          imm = int(random.random()*2**32) 
       line = str(asm_line[0]) + "\t $" + str(r0) + ",\t " + str(imm)
@@ -118,3 +123,4 @@ for i in range(0, 10):
 #    test_file.write(label_str)
 #    test_file.write("\n")
 
+test_file.close()
--- sim/random_gen/weights.py
+++ sim/random_gen/weights.py
@@ -1,107 +1,125 @@
-# import opcodes
+import random
 
-prob_opcode_add     = 10
-prob_opcode_addi    = 10
-prob_opcode_addiu   = 10
-prob_opcode_addu    = 10
-prob_opcode_sub     = 10
-prob_opcode_subu    = 10
-prob_opcode_and     = 10
-prob_opcode_andi    = 10
-prob_opcode_nor     = 10
-prob_opcode_or      = 10
-prob_opcode_ori     = 10
-prob_opcode_xor     = 10
-prob_opcode_xori    = 10
-prob_opcode_sll     = 10
-prob_opcode_srl     = 10
-prob_opcode_sra     = 10
-prob_opcode_sllv    = 10
-prob_opcode_srlv    = 10
-prob_opcode_srav    = 10
-prob_opcode_slt     = 10
-prob_opcode_slti    = 10
-prob_opcode_sltiu   = 10
-prob_opcode_sltu    = 10
-prob_opcode_beq     =  0
-prob_opcode_bne     =  0
-prob_opcode_blt     =  0
-prob_opcode_bgt     =  0
-prob_opcode_ble     =  0
-prob_opcode_bge     =  0
-prob_opcode_j       =  0
-prob_opcode_jal     =  0
-prob_opcode_jr      =  0
-prob_opcode_jalr    =  0
-prob_opcode_move    = 10
-prob_opcode_lb      =  0
-prob_opcode_lbu     =  0
-prob_opcode_lh      =  0
-prob_opcode_lhu     =  0
-prob_opcode_lui     =  0
-prob_opcode_lw      =  0
-prob_opcode_li      =  0
-prob_opcode_la      =  0
-prob_opcode_sb      =  0
-prob_opcode_sh      =  0
-prob_opcode_sw      =  0
+class cell:
+   # weight can be le then max or ge then min
+   # so max and min are feasible value for weight
+   def __init__(self, v_min, v_max, v_weight = 0) :
+      self.value_min = v_min
+      self.value_max = v_max
+      self.weight    = v_weight
+   def set (self, v_weight) :
+      self.weight    = v_weight
+
+test_weights_d = {}
+
+test_weights_d["prob_opcode_add"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_addi"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_addiu"]   = cell(0, 100, 10)
+test_weights_d["prob_opcode_addu"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_sub"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_subu"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_and"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_andi"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_nor"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_or"]      = cell(0, 100, 10)
+test_weights_d["prob_opcode_ori"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_xor"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_xori"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_sll"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_srl"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_sra"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_sllv"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_srlv"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_srav"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_slt"]     = cell(0, 100, 10)
+test_weights_d["prob_opcode_slti"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_sltiu"]   = cell(0, 100, 10)
+test_weights_d["prob_opcode_sltu"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_beq"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_bne"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_blt"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_bgt"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_ble"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_bge"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_j"]       = cell(0,   0,  0)
+test_weights_d["prob_opcode_jal"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_jr"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_jalr"]    = cell(0,   0,  0)
+test_weights_d["prob_opcode_move"]    = cell(0, 100, 10)
+test_weights_d["prob_opcode_lb"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_lbu"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_lh"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_lhu"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_lui"]     = cell(0,   0,  0)
+test_weights_d["prob_opcode_lw"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_li"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_la"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_sb"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_sh"]      = cell(0,   0,  0)
+test_weights_d["prob_opcode_sw"]      = cell(0,   0,  0)
 
 # must be greater than three
-numb_of_regs_to_use = 10
+test_weights_d["numb_of_regs_to_use_max_value"] = 32
+test_weights_d["numb_of_regs_to_use"] = random.choice([i for i in range (3, test_weights_d["numb_of_regs_to_use_max_value"]+1)])
 
 # imm_value_5
-imm_value_5_l = [ i for i in range (0, 2**5) ]
-
-# imm_value_16
-imm_value_16_l = [ i for i in range (0, 2**16) ]
+test_weights_d["imm_value_5_max_value"] = 2**5+1
+test_weights_d["imm_value_5_l"] = [i for i in range (0, test_weights_d["imm_value_5_max_value"])]
 # imm_value_15
-imm_value_15_l = [ i for i in range (0, 2**15) ]
+test_weights_d["imm_value_15_max_value"] = 2**15+1
+test_weights_d["imm_value_15_l"] = [i for i in range (0, test_weights_d["imm_value_15_max_value"])]
+# imm_value_16
+test_weights_d["imm_value_16_max_value"] = 2**15+1
+test_weights_d["imm_value_16_l"] = [i for i in range (0, test_weights_d["imm_value_16_max_value"])]
 
 # between 1 and 100
 max_num_of_labels = 1
 
-weighted_opcodes_l = (prob_opcode_add    * ["add"] +  
-                      prob_opcode_addi   * ["addi"] +
-                      prob_opcode_addiu  * ["addiu"] +
-                      prob_opcode_addu   * ["addu"] +
-                      prob_opcode_sub    * ["sub"] +
-                      prob_opcode_subu   * ["subu"] +
-                      prob_opcode_and    * ["and"] +
-                      prob_opcode_andi   * ["andi"] +
-                      prob_opcode_nor    * ["nor"] +
-                      prob_opcode_or     * ["or"] +
-                      prob_opcode_ori    * ["ori"] +
-                      prob_opcode_xor    * ["xor"] +
-                      prob_opcode_xori   * ["xori"] +
-                      prob_opcode_sll    * ["sll"] +
-                      prob_opcode_srl    * ["srl"] +
-                      prob_opcode_sra    * ["sra"] +
-                      prob_opcode_sllv   * ["sllv"] +
-                      prob_opcode_srlv   * ["srlv"] +
-                      prob_opcode_srav   * ["srav"] +
-                      prob_opcode_slt    * ["slt"] +
-                      prob_opcode_slti   * ["slti"] +
-                      prob_opcode_sltiu  * ["sltiu"] +
-                      prob_opcode_sltu   * ["sltu"] +
-                      prob_opcode_beq    * ["beq"] +
-                      prob_opcode_bne    * ["bne"] +
-                      prob_opcode_blt    * ["blt"] +
-                      prob_opcode_bgt    * ["bgt"] +
-                      prob_opcode_ble    * ["ble"] +
-                      prob_opcode_bge    * ["bge"] +
-                      prob_opcode_j      * ["j"] +
-                      prob_opcode_jal    * ["jal"] +
-                      prob_opcode_jr     * ["jr"] +
-                      prob_opcode_jalr   * ["jalr"] +
-                      prob_opcode_move   * ["move"] +
-                      prob_opcode_lb     * ["lb"] +
-                      prob_opcode_lbu    * ["lbu"] +
-                      prob_opcode_lh     * ["lh"] +
-                      prob_opcode_lhu    * ["lhu"] +
-                      prob_opcode_lui    * ["lui"] +
-                      prob_opcode_lw     * ["lw"] +
-                      prob_opcode_li     * ["li"] +
-                      prob_opcode_la     * ["la"] +
-                      prob_opcode_sb     * ["sb"] +
-                      prob_opcode_sh     * ["sh"] +
-                      prob_opcode_sw     * ["sw"])
+# list from which chose an instruction
+def set_list() :
+   global weighted_opcodes_l 
+   weighted_opcodes_l = (test_weights_d["prob_opcode_add"].weight    * ["add"]   +  
+                         test_weights_d["prob_opcode_addi"].weight   * ["addi"]  +
+                         test_weights_d["prob_opcode_addiu"].weight  * ["addiu"] +
+                         test_weights_d["prob_opcode_addu"].weight   * ["addu"]  +
+                         test_weights_d["prob_opcode_sub"].weight    * ["sub"]   +
+                         test_weights_d["prob_opcode_subu"].weight   * ["subu"]  +
+                         test_weights_d["prob_opcode_and"].weight    * ["and"]   +
+                         test_weights_d["prob_opcode_andi"].weight   * ["andi"]  +
+                         test_weights_d["prob_opcode_nor"].weight    * ["nor"]   +
+                         test_weights_d["prob_opcode_or"].weight     * ["or"]    +
+                         test_weights_d["prob_opcode_ori"].weight    * ["ori"]   +
+                         test_weights_d["prob_opcode_xor"].weight    * ["xor"]   +
+                         test_weights_d["prob_opcode_xori"].weight   * ["xori"]  +
+                         test_weights_d["prob_opcode_sll"].weight    * ["sll"]   +
+                         test_weights_d["prob_opcode_srl"].weight    * ["srl"]   +
+                         test_weights_d["prob_opcode_sra"].weight    * ["sra"]   +
+                         test_weights_d["prob_opcode_sllv"].weight   * ["sllv"]  +
+                         test_weights_d["prob_opcode_srlv"].weight   * ["srlv"]  +
+                         test_weights_d["prob_opcode_srav"].weight   * ["srav"]  +
+                         test_weights_d["prob_opcode_slt"].weight    * ["slt"]   +
+                         test_weights_d["prob_opcode_slti"].weight   * ["slti"]  +
+                         test_weights_d["prob_opcode_sltiu"].weight  * ["sltiu"] +
+                         test_weights_d["prob_opcode_sltu"].weight   * ["sltu"]  +
+                         test_weights_d["prob_opcode_beq"].weight    * ["beq"]   +
+                         test_weights_d["prob_opcode_bne"].weight    * ["bne"]   +
+                         test_weights_d["prob_opcode_blt"].weight    * ["blt"]   +
+                         test_weights_d["prob_opcode_bgt"].weight    * ["bgt"]   +
+                         test_weights_d["prob_opcode_ble"].weight    * ["ble"]   +
+                         test_weights_d["prob_opcode_bge"].weight    * ["bge"]   +
+                         test_weights_d["prob_opcode_j"].weight      * ["j"]     +
+                         test_weights_d["prob_opcode_jal"].weight    * ["jal"]   +
+                         test_weights_d["prob_opcode_jr"].weight     * ["jr"]    +
+                         test_weights_d["prob_opcode_jalr"].weight   * ["jalr"]  +
+                         test_weights_d["prob_opcode_move"].weight   * ["move"]  +
+                         test_weights_d["prob_opcode_lb"].weight     * ["lb"]    +
+                         test_weights_d["prob_opcode_lbu"].weight    * ["lbu"]   +
+                         test_weights_d["prob_opcode_lh"].weight     * ["lh"]    +
+                         test_weights_d["prob_opcode_lhu"].weight    * ["lhu"]   +
+                         test_weights_d["prob_opcode_lui"].weight    * ["lui"]   +
+                         test_weights_d["prob_opcode_lw"].weight     * ["lw"]    +
+                         test_weights_d["prob_opcode_li"].weight     * ["li"]    +
+                         test_weights_d["prob_opcode_la"].weight     * ["la"]    +
+                         test_weights_d["prob_opcode_sb"].weight     * ["sb"]    +
+                         test_weights_d["prob_opcode_sh"].weight     * ["sh"]    +
+                         test_weights_d["prob_opcode_sw"].weight     * ["sw"])
