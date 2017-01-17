ea31dfcfa05c1f2afc5fe07355aa89516fafe8a9
alberto guasco <albebirio@gmail.com>
2017-01-17 22:53:54 +0100

    fix in the set_list method

--- sim/random_gen/weights.py
+++ sim/random_gen/weights.py
@@ -1,4 +1,5 @@
 import random
+import pdb
 
 class cell:
    # weight can be le then max or ge then min
@@ -74,52 +75,20 @@ max_num_of_labels = 1
 
 # list from which chose an instruction
 def set_list() :
-   global weighted_opcodes_l 
-   weighted_opcodes_l = (test_weights_d["prob_opcode_add"].weight    * ["add"]   +  
-                         test_weights_d["prob_opcode_addi"].weight   * ["addi"]  +
-                         test_weights_d["prob_opcode_addiu"].weight  * ["addiu"] +
-                         test_weights_d["prob_opcode_addu"].weight   * ["addu"]  +
-                         test_weights_d["prob_opcode_sub"].weight    * ["sub"]   +
-                         test_weights_d["prob_opcode_subi"].weight   * ["subu"]  +
-                         test_weights_d["prob_opcode_and"].weight    * ["and"]   +
-                         test_weights_d["prob_opcode_andi"].weight   * ["andi"]  +
-                         test_weights_d["prob_opcode_nor"].weight    * ["nor"]   +
-                         test_weights_d["prob_opcode_or"].weight     * ["or"]    +
-                         test_weights_d["prob_opcode_ori"].weight    * ["ori"]   +
-                         test_weights_d["prob_opcode_xor"].weight    * ["xor"]   +
-                         test_weights_d["prob_opcode_xori"].weight   * ["xori"]  +
-                         test_weights_d["prob_opcode_sll"].weight    * ["sll"]   +
-                         test_weights_d["prob_opcode_srl"].weight    * ["srl"]   +
-                         test_weights_d["prob_opcode_sra"].weight    * ["sra"]   +
-                         test_weights_d["prob_opcode_sllv"].weight   * ["sllv"]  +
-                         test_weights_d["prob_opcode_srlv"].weight   * ["srlv"]  +
-                         test_weights_d["prob_opcode_srav"].weight   * ["srav"]  +
-                         test_weights_d["prob_opcode_slt"].weight    * ["slt"]   +
-                         test_weights_d["prob_opcode_slti"].weight   * ["slti"]  +
-                         test_weights_d["prob_opcode_sltiu"].weight  * ["sltiu"] +
-                         test_weights_d["prob_opcode_sltu"].weight   * ["sltu"]  +
-                         test_weights_d["prob_opcode_beq"].weight    * ["beq"]   +
-                         test_weights_d["prob_opcode_bne"].weight    * ["bne"]   +
-                         test_weights_d["prob_opcode_blt"].weight    * ["blt"]   +
-                         test_weights_d["prob_opcode_bgt"].weight    * ["bgt"]   +
-                         test_weights_d["prob_opcode_ble"].weight    * ["ble"]   +
-                         test_weights_d["prob_opcode_bge"].weight    * ["bge"]   +
-                         test_weights_d["prob_opcode_j"].weight      * ["j"]     +
-                         test_weights_d["prob_opcode_jal"].weight    * ["jal"]   +
-                         test_weights_d["prob_opcode_jr"].weight     * ["jr"]    +
-                         test_weights_d["prob_opcode_jalr"].weight   * ["jalr"]  +
-                         test_weights_d["prob_opcode_move"].weight   * ["move"]  +
-                         test_weights_d["prob_opcode_lb"].weight     * ["lb"]    +
-                         test_weights_d["prob_opcode_lbu"].weight    * ["lbu"]   +
-                         test_weights_d["prob_opcode_lh"].weight     * ["lh"]    +
-                         test_weights_d["prob_opcode_lhu"].weight    * ["lhu"]   +
-                         test_weights_d["prob_opcode_lui"].weight    * ["lui"]   +
-                         test_weights_d["prob_opcode_lw"].weight     * ["lw"]    +
-                         test_weights_d["prob_opcode_li"].weight     * ["li"]    +
-                         test_weights_d["prob_opcode_la"].weight     * ["la"]    +
-                         test_weights_d["prob_opcode_sb"].weight     * ["sb"]    +
-                         test_weights_d["prob_opcode_sh"].weight     * ["sh"]    +
-                         test_weights_d["prob_opcode_sw"].weight     * ["sw"])
+   global weighted_opcodes_l
+
+   weighted_opcodes_l = []
+   for item in test_weights_d.keys():
+      if "multi" in item:
+         if (test_weights_d[item].weight != 0):
+            weighted_opcodes_l.extend(test_weights_d[item].weight * [item]);
+      elif "prob" in item: 
+         instr = item.split("_")[-1]
+         if (test_weights_d[item].weight != 0):
+            # pdb.set_trace()
+            weighted_opcodes_l.extend(test_weights_d[item].weight * [instr]);
+
+ 
    test_weights_d["imm_value_5_l"] = [i for i in range (0, test_weights_d["imm_value_5_max_value"])]
    test_weights_d["imm_value_15_l"] = [i for i in range (0, test_weights_d["imm_value_15_max_value"])]
    test_weights_d["imm_value_16_l"] = [i for i in range (0, test_weights_d["imm_value_16_max_value"])]
